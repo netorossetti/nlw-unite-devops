b9afc92a39d1f70f378acbee6ff6c88b0e124047
Neto Rossetti <netinho.rossetti@gmail.com>
2024-04-04 16:20:06 -0300

    feat: create health route

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,7 @@ import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
 import { errorHandler } from "./error-handler";
+import { health } from "./routes/health";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -51,6 +52,7 @@ app.register(getEvent);
 app.register(getAttendeeBadge);
 app.register(checkIn);
 app.register(getEventAttendees);
+app.register(health);
 
 app.setErrorHandler(errorHandler);
 
